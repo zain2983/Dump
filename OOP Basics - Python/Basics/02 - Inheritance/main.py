f7dc168f46060033b0fb879e1314b135b44cd2a8
Zain Al Abidin <113837968+zain2983@users.noreply.github.com>
2024-08-28 22:29:48 +0500

    Basic Concepts of OOP

---
+++ OOP Basics - Python/Basics/02 - Inheritance/main.py
@@ -0,0 +1,14 @@
+from car import Car
+from car import ElectricCar
+
+car_1 = Car("Chevy","Corvette")
+car_2 = Car("Ford","Mustang")
+car_3 = ElectricCar("Tesla","Model 3","85KWH")
+print(car_3.show())
+
+
+# car_1.drive()
+# car_1.stop()
+
+# car_2.stop()
+# car_2.drive() 
